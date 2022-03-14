c5bc46fc63119b5d270d534771d2177ec4c8d70a
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2022-03-14 09:57:29 -0300

    Refatoracao da adicao de estilos

--- src/components/Field.js
+++ src/components/Field.js
@@ -14,9 +14,9 @@ class Field extends Component {
     
     exploded ? styleField.push(styles.exploded) : null;
 
-    flagged ? styleField.push(styles.flagged, styles.regular) : null;
+    flagged ? styleField.push(styles.flagged) : null;
 
-    ( styleField.length === 1 ) ? styleField.push(styles.regular) : null;
+    ( !opened && !exploded ) ? styleField.push(styles.regular) : null;
 
     if (nearMines > 0) {
       if (nearMines === 1 ) color = '#2A28D7' 
